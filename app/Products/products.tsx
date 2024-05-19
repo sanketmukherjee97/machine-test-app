ba0abe58dbb473c8b294a84c9da7c52e6cf9c70b
sanketmukherjee97 <sanketmukherjee189official@gmail.com>
2024-05-19 14:56:16 +0530

    dragging

--- app/Products/products.tsx
+++ app/Products/products.tsx
@@ -6,6 +6,11 @@ import { Grid } from "@mui/material";
 import ProductCards from "../components/ProductCards/ProductCards";
 import { ProductType } from "../apicalls/apicalls";
 import { DndContext, closestCenter } from "@dnd-kit/core";
+import {
+  horizontalListSortingStrategy,
+  verticalListSortingStrategy,
+  SortableContext,
+} from "@dnd-kit/sortable";
 
 const Products = () => {
   const [products, setProducts]: any = useState([]);
@@ -20,41 +25,64 @@ const Products = () => {
       });
   }, []);
 
-  const handleDragEnd = () => {};
+  useEffect(() => {
+    console.log(selectedCard);
+  }, [selectedCard]);
+
+  const handleDragEnd = (event: any) => {
+    const { active, over } = event;
+    let pro = products;
+    let temp;
+    console.log("Dreaged", active, over);
+    setSelectedCard(over?.id?.toString());
+    temp = pro[parseInt(active?.id)];
+    pro[parseInt(active?.id)] = pro[parseInt(over?.id)];
+    pro[parseInt(over?.id)] = temp;
+    setProducts(pro);
+  };
+
+  const handleDragStart = (event: any) => {
+    const { active, over } = event;
+    setSelectedCard(active?.id?.toString());
+  };
 
   return (
     <div className={styles.products}>
       <h1 className={styles.heading}>Next.js Machine Test</h1>
       {products.length !== 0 ? (
         <div className={styles.productViewer}>
-          <DndContext
-            collisionDetection={closestCenter}
-            onDragEnd={handleDragEnd}
-          >
-            <Grid container spacing={2}>
-              {products?.map((item: ProductType, index: number) => (
-                <Grid
-                  item
-                  xs={2}
-                  sm={4}
-                  md={4}
-                  key={index}
-                  className={styles.grid}
-                >
-                  <ProductCards
-                    title={item?.title}
-                    price={item?.price}
-                    desc={item?.description}
-                    Image={item?.image}
-                    showCard={item?.id === selectedCard}
-                    onClick={() => {
-                      setSelectedCard(item?.id);
-                    }}
-                  />
-                </Grid>
-              ))}
-            </Grid>
-          </DndContext>
+          <Grid container spacing={2}>
+            <DndContext
+              collisionDetection={closestCenter}
+              onDragStart={handleDragStart}
+              onDragEnd={handleDragEnd}
+            >
+              <SortableContext
+                items={products}
+                strategy={verticalListSortingStrategy}
+              >
+                {products?.map((item: ProductType, index: number) => (
+                  <Grid
+                    item
+                    xs={2}
+                    sm={4}
+                    md={4}
+                    key={index}
+                    className={styles.grid}
+                  >
+                    <ProductCards
+                      id={index.toString()}
+                      title={item?.title}
+                      price={item?.price}
+                      desc={item?.description}
+                      Image={item?.image}
+                      showCard={index?.toString() === selectedCard}
+                    />
+                  </Grid>
+                ))}
+              </SortableContext>
+            </DndContext>
+          </Grid>
         </div>
       ) : (
         <p>No Data</p>
--- app/components/ProductCards/ProductCards.tsx
+++ app/components/ProductCards/ProductCards.tsx
@@ -5,8 +5,11 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import styles from "./ProductCards.module.css";
 import Typography from "@mui/material/Typography";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 interface ProductCardType {
+  id?: string;
   title?: string;
   price?: number;
   desc?: string;
@@ -15,26 +18,42 @@ interface ProductCardType {
   onClick?: () => void;
 }
 
-const ProductCards = (props: ProductCardType) => {
+const ProductCards = ({ id = "0", ...props }: ProductCardType) => {
+  const { attributes, listeners, setNodeRef, transform, transition } =
+    useSortable({ id: id });
+
+  const style = {
+    transform: CSS.Transform.toString(transform),
+    transition,
+  };
   return (
-    <Card
-      sx={{ width: "100%", height: "100%", position: "relative" }}
-      onClick={props?.onClick}
+    <div
+      className={styles.card}
+      ref={setNodeRef}
+      style={style}
+      {...attributes}
+      {...listeners}
     >
-      <CardMedia sx={{ height: 140 }} image={props.Image} title={props.title} />
-      <CardContent>
-        <Typography gutterBottom variant="h5" component="div">
-          {props.title}
-        </Typography>
-        <Typography gutterBottom variant="h6" component="div">
-          {props.price}
-        </Typography>
-        <Typography variant="body2" color="text.secondary">
-          {props.desc}
-        </Typography>
-      </CardContent>
-      {!props?.showCard ? <div className={styles.blackCover}></div> : null}
-    </Card>
+      <Card sx={{ width: "100%", height: "100%", position: "relative" }}>
+        <CardMedia
+          sx={{ height: 140 }}
+          image={props.Image}
+          title={props.title}
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="div">
+            {props.title}
+          </Typography>
+          <Typography gutterBottom variant="h6" component="div">
+            {props.price}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            {props.desc}
+          </Typography>
+        </CardContent>
+        {!props?.showCard ? <div className={styles.blackCover}></div> : null}
+      </Card>
+    </div>
   );
 };
 
